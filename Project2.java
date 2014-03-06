a0bdd5b85acd3fab7beef1ca1b49d45db0163420
13122614 <13122614@studentmail.ul.ie>
2014-03-06 20:11:31 +0000

    Added methods to open files and create calanders.
    Added methods to construct files (for existing files only) and Gregorian calendar objects.

--- Project2.java
+++ Project2.java
@@ -3,45 +3,44 @@ public class Project2{
 		
 		
 	}
-	public static int toDays(String toConvert){
+	public static File openFile(String fileName){
 		/*
-			Converts a String in the form DD/MM/YYYY to the number of days
-			since January 1st 1970.
-			Returns intMin in the case of an error.
+			Returns a File object if the desired file exists in the current directory.
+			Prints an error message and terminates if the file does not exist.
+		*/
+		File openedFile;
+		openedFile = new File(fileName);
+		if (!openedFile.exists()){
+			System.err.println("The given file does not exist");
+			System.exit(74);
+		}
+		return openedFile;
+
+	}
+
+
+
+	public static GregorianCalendar toCalendar(String toConvert){
+		/*
+			Converts a String in the form DD/MM/YYYY to a Gregorian Calendar object.
+			Returns a default GregorianCalendar in the case of an error.
 		*/
 		final String validDatePattern = "\\d{1,2}/\\d{1,2}/\\d{4}";
 		String[] dayMonthYearStrings;
 		int[] dayMonthYearValues = new int[3];
-		int numberOfDays;
-		GregorianCalendar dateToConvert;
+		GregorianCalendar dateOutput = new GregorianCalendar();
 		if (toConvert.matches(validDatePattern)) {
 			dayMonthYearStrings = toConvert.split("/");
 			dayMonthYearValues = arrayToInt(dayMonthYearStrings);
-			dateToConvert = new GregorianCalendar(dayMonthYearValues[2], dayMonthYearValues[1]-1, dayMonthYearValues[0]);
-			numberOfDays = toDays(dateToConvert.getTimeInMillis());
+			if (dayMonthYearValues.length == 3) {
+				dateOutput = new GregorianCalendar(dayMonthYearValues[2], dayMonthYearValues[1]-1, dayMonthYearValues[0]);	
+			}
+			
 		}
-		else numberOfDays = -2147483648;
-		return numberOfDays;
+		return dateOutput;
 	}
-	public static int toDays(Date toConvert){
-		/*
-			Converts a Date object into days since January 1970.
-			Returns an int value.
-		*/
-		int numberOfDays; long milliseconds;
-		milliseconds = toConvert.getTime();
-		numberOfDays = toDays(milliseconds);
-		return numberOfDays;
-	}
-	public static int toDays(long toConvert){
-		/*
-			Converts time in milliseconds into days since January 1st 1970.
-			Returns an int value.
-		*/
-		int numberOfDays;
-		numberOfDays = (int) (toConvert / 86400000L);
-		return numberOfDays;
-	}
+
+
 	public static int[] arrayToInt(String[] numberArray){
 		/*
 			Converts an array of Strings into an array of ints.
@@ -61,6 +60,8 @@ public class Project2{
 		if (someError) intArray = new int[0];
 		return intArray;
 	}
+
+
 	public static byte[] arrayToByte(String[] numberArray){
 		/*
 			Converts an array of Strings into an array of bytes.
@@ -80,5 +81,6 @@ public class Project2{
 		if (someError) byteArray = new byte[0];
 		return byteArray;
 	}
+
 
 }
