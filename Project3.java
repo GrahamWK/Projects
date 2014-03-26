a95d1ff694fcd98eca84e1bb7ed902e5c2c5ce57
13122614 <13122614@studentmail.ul.ie>
2014-03-26 14:53:25 +0000

    Unfinished alterations to Project3
    github doesn't have an option for staging changes, so this will have to do.
    Anyone looking at history will already be pissed off with the revisions anyway, so myaa.

--- Project3.java
+++ Project3.java
@@ -1,21 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class Project3{
 	/**
 		A program to demonstrate string manipulation and handling files.
-		Effort was made to have only one exit point in the code, returning an exit state would probably be a better idea though.
-		I'm also assuming that the main method is meant do deal with anything but selecting the method to execute.
 		Spelling in this code is compliant with American English.
 	**/
-	//This Scanner object is declared globally because two methods require it.
+	//This Scanner object is declared globally because multiple methods require it.
 	public static Scanner consoleInput = new Scanner(System.in);
+	public static ArrayList<String> userDictArrayList;
 	public static void main(String[] args) {
+		File userDictFile;
 		byte menuChoice = -1;
 		if (args.length > 0) {
 			if (args[0].matches("-h") || args[0].matches("--help")) {
-				menuChoice = 0;
 				System.out.println("This program allows users to interact with a dictionary file");
+				System.exit(0);
 			}
 		}
+		String dictFileName = "";
+		System.out.println("Please enter the name of your dictionary file.");
+		dictFileName = consoleInput.nextLine();
+		userDictFile = new File(dictFileName);
+		if (userDictFile.exists()){
+			userDictArrayList = readFile(dictFileName);
+		} else {
+			userDictArrayList = new ArrayList<String>();
+			System.out.println()
+		}
+
 		while (menuChoice != 0){
 			menuChoice = getMenuOption();
 			/*
@@ -54,7 +66,7 @@ public class Project3{
 		menuOptions += "\n 3) Delete.";
 		menuOptions += "\n 4) Frequency of starting letters and word count";
 		menuOptions += "\n 5) Longest, shortest word and palindrome.";
-		menuOptions += "\n 6) Add passage";
+		menuOptions += "\n 6) Add passage from file";
 		menuOptions += "\n 7) Lexicographer function";
 		menuOptions += "\n 0) Exit";
 		/*
@@ -77,19 +89,33 @@ public class Project3{
 		return chosenOption;
 	}
 	
-public static void SearchDictionary(){}
+	public static void SearchDictionary(){}
 
-public static void AddDictionary(){}
-
-public static void DeleteDictionary(){}
-
-public static void FrequencyWordCount(){}
-
-public static void LongesyShortestPalindrome(){}
-
-public static void AddPassage(){}
-
-public static void Lexicographer(){}
+	public static void AddDictionary(){}
 	
+	public static void DeleteDictionary(){}
+	
+	public static void FrequencyWordCount(){}
+	
+	public static void LongesyShortestPalindrome(){}
+	
+	public static void AddPassage(){
+
+}
+
+	public static void Lexicographer(){}
+	
+
+
+	public static ArrayList<String> readFile(String fileName){
+		/*
+			Accepts a string denoting the relative position of a text file.
+			Returns an ArrayList of Strings with a single word in each, including special characters.
+		*/
+		File fileToRead = new File(fileName);
+		if (!fileToRead.exists()){}
+		ArrayList<String> fileWordsArrayList = new ArrayList<String>();
+		return fileWordsArrayList;
+	}
 }
 	
