c25217d8e72ddc972b593aa16ae6188ce6e69bd3
13122614 <13122614@studentmail.ul.ie>
2014-04-02 11:17:00 +0100

    Create Fixture Class.
    The class has no use as of yet, but compiles and runs.
    Basically, the skeleton of the program.

---
+++ Project4/Fixture.java
@@ -0,0 +1,19 @@
+public class Fixture{
+	/**
+		The Fixture class describes an object containing information on a premiership match fixture.
+		This class can be constructed as either a past, or future match.
+		Objects of this class can easily be displayed using print methods.
+	**/
+
+	private boolean hasBeenPlayed;
+	private int teamNumber;
+
+	public String toString(){
+	/*
+		This method replaces the toString method to allow direct printing of the object.
+		Will return formatted results for the instance of fixture.
+	*/
+
+		return "fuzzy dice";
+	}
+}
