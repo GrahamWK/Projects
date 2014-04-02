a826f60e871581efa138e96379cadc60f7ddb720
13122614 <13122614@studentmail.ul.ie>
2014-04-02 13:30:37 +0200

    Added a static method to read files.
    Added a method (completely broken at this point) to read the lines in files.
    The intention is to only read the file with team numbers and names once and use the same data for every constructor.

--- Project4/Fixture.java
+++ Project4/Fixture.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.io.*;
 public class Fixture{
 	/**
 		The Fixture class describes an object containing information on a premiership match fixture.
@@ -6,7 +8,9 @@ public class Fixture{
 	**/
 
 	private boolean hasBeenPlayed;
-	private int teamNumber;
+	private int homeTeamNumber, awayTeamNumber;
+	//This file name should not be written here in any serious application.
+	private final String[] teamNamesAndNumbers = readLinesFromFile("PremiershipTeamsOrPlayers.txt");
 
 	public String toString(){
 	/*
@@ -15,5 +19,39 @@ public class Fixture{
 	*/
 
 		return "fuzzy dice";
+	}
+
+	/**
+	************************************************
+	This method does not currently work correctly.
+	It will be fixed in timer to use it in one of the constructors
+	**/
+	private static String[] readLinesFromFile(String fileName){
+		/*
+			Accepts a string denoting the relative position of a text file.
+			Returns an Array of Strings with a single line in each.
+		*/
+		File fileToRead = new File(fileName);
+		Scanner dataFromFile;
+		ArrayList<String> fileLinesArrayList = new ArrayList<String>();
+		String[] linesFromFile;
+		try {
+				if (!fileToRead.exists()){
+					dataFromFile = new Scanner(fileToRead);
+					while (dataFromFile.hasNext()) {
+						//Add every line from the file to the ArrayList.
+						fileLinesArrayList.add(dataFromFile.nextLine());
+					}
+				}
+		}
+		catch (Exception e){
+			//If the file doesn't exist and logic fails, empty out the ArrayList.
+			fileLinesArrayList = new ArrayList<String>();
+		}
+		//Create an Array large enough to hold all of the lines from the file.
+		linesFromFile = new String[fileLinesArrayList.size()];
+		//Copy every line from the ArrayList to the Array.
+		for (int i = 0; i < linesFromFile.length; ++i) linesFromFile[i] = fileLinesArrayList.get(i);
+		return linesFromFile;
 	}
 }
