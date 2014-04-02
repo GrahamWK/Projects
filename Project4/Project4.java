5ee2918dc853c45e50ae6d2253b4cad7d8df0de5
13122614 <13122614@studentmail.ul.ie>
2014-04-02 11:14:49 +0100

    Create rough Draft of the main method.
    Created this to test out the toString() method in the Fixtures class.

--- Project4/Project4.java
+++ Project4/Project4.java
@@ -1,3 +1,10 @@
 public class Project4{
-
+	/**
+		This program allows a user to view Premiership results and fixtures and display a table with all available results.
+		Fixture information, results and a table of team names and numbers are stored in the same folder as CSV.
+	**/
+	public static void main(String[] args) {
+		Fixture f1 = new Fixture();
+		System.out.print(f1);
+	}
 }
