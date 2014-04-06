fd00c77befaac5faee91afe42d9be80e90693832
13122614 <13122614@studentmail.ul.ie>
2014-04-06 21:07:23 +0100

    Added readFile() method.
    Added a method to read lines from files without throwing any exceptions.

--- Project4/Project4.java
+++ Project4/Project4.java
@@ -1,10 +1,46 @@
+import java.io.*;
+import java.util.*;
 public class Project4{
 	/**
 		This program allows a user to view Premiership results and fixtures and display a table with all available results.
 		Fixture information, results and a table of team names and numbers are stored in the same folder as CSV.
 	**/
 	public static void main(String[] args) {
-		Fixture f1 = new Fixture();
-		System.out.print(f1);
+
+
+	}
+
+	private static ArrayList<String> readFile(String filename){
+		/*
+			Reads a file in the same directory.
+			Returns an ArrayList with each of the lines from the file.
+			Produces an error message if the file is not found and returns an empty ArrayList.
+		*/
+		Scanner fileData;
+		ArrayList<String> toReturn = new ArrayList<String>();
+		File fileToRead = new File(filename);
+		//Check if the desired file does not exist.
+		if (!fileToRead.exists()){
+			System.err.println("The file \"" + filename + "\" was not found.");
+		}else{
+			try{
+				//Construct a Scanner and read all of the lines in the file.
+				fileData = new Scanner(fileToRead);
+				while (fileData.hasNext())
+					toReturn.add(fileData.nextLine());
+				fileData.close();
+			}
+			/*
+				Catch any and all exceptions that occour when reading the file,
+				print some friendly debug information to the screen,
+				and construct an empty ArrayList to return.
+			*/
+			catch (Exception e) {
+				System.err.println("A file exception occoured, give this to a developer:\n\n" + e + "\nOn file: " + filename);
+				toReturn = new ArrayList<String>();
+			}
+		}
+		return toReturn;
+
 	}
 }
